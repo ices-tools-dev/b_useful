e0fcdc26652ca982d3b9adb9c3c94582efade05d
Neil Maginnis <neil.maginnis@ices.dk>
2026-03-13 14:43:01 +0100

    tab for viewing metrics at different time points by filtering data and facet plotting

--- R/mod_wp3_time_comparison.R
+++ R/mod_wp3_time_comparison.R
@@ -10,44 +10,27 @@
 mod_wp3_time_comparison_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    card(layout_sidebar(sidebar = 
+    card(height = "75vh",
+         layout_sidebar(sidebar = 
                           sidebar(radioButtons(ns("diversity_idx"), label = "Select diversity index", choices = c("Species Richness" = "Richness", "Evenness" = "evenness", "Shannon Index" = "shannon", "Functional Richness" = "fric", "Functional Evenness" = "feve", "Functional Dispersion" = "fdis", "Functional Diversity" = "fdiv")),
                                   selectizeInput(inputId = ns("year_selector"), "Select Year(s)",
                                                  multiple = TRUE,
                                                  choices = 2010:2020,
                                                  selected = c("2010", "2015", "2020")),
-                                  sliderInput(ns("point_size"), label = "Adjust point size in plot", min = 0.75, max = 2.5, value = 0.75)),
-                        tags$script(sprintf('
-        var dimension = [0, 0];
-        $(document).on("shiny:connected", function(e) {
-          dimension[0] = window.innerWidth;
-          dimension[1] = window.innerHeight;
-          Shiny.onInputChange("%s", dimension);
-        });
-        $(window).resize(function(e) {
-          dimension[0] = window.innerWidth;
-          dimension[1] = window.innerHeight;
-          Shiny.onInputChange("%s", dimension);
-        });
-      ', ns("dimension"), ns("dimension"))),
-                        verbatimTextOutput(ns("dimension_display")),
-                        plotOutput(ns("div_plot"),
-                                   height = "75vh")))
+                                  sliderInput(ns("point_size"), label = "Adjust point size in plot", min = 0.75, max = 2.5, value = 1.5, round = -1)),
+                        card_body(padding = 0,
+                                  verbatimTextOutput(ns("dimension_display")),
+                                  plotOutput(ns("div_plot")))))
   )
 }
     
 #' wp3_time_comparison Server Functions
 #'
 #' @noRd 
-mod_wp3_time_comparison_server <- function(id, map_parameters){
+mod_wp3_time_comparison_server <- function(id, map_parameters, case_study){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
- 
-    output$dimension_display <- renderText({
-      req(input$dimension)
-      paste(input$dimension[1], input$dimension[2], input$dimension[2]/input$dimension[1])
-    })
-    
+
     reactive_data <- reactive({
       req(fish_diversity)
       req(input$year_selector)
@@ -63,7 +46,7 @@ mod_wp3_time_comparison_server <- function(id, map_parameters){
       req(map_parameters())
       
       p <- ggplot() +
-        geom_point(data = reactive_data(), aes(x = longitude, y = latitude, color = !!rlang::sym(input$diversity_idx)), size = input$dimension[1]/1250) +
+        geom_point(data = reactive_data(), aes(x = longitude, y = latitude, color = !!rlang::sym(input$diversity_idx)), size = input$point_size) +
         scale_color_gradientn(colours = rev(brewer.pal(11, "RdYlBu")))+
         geom_sf(data = map_shape, fill = "grey")+
         scale_x_continuous(breaks= map_parameters()$coordxmap)+
