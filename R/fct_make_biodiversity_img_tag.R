5ebd2a10880f33f6b2fd3f5c693ecaea68ced5f0
Neil Maginnis <neil.maginnis@ices.dk>
2026-03-13 14:40:22 +0100

    implement interactive tab for viewing biodiversity metrics, selecting points from map and viewing detail and summary tables

---
+++ R/fct_make_biodiversity_img_tag.R
@@ -0,0 +1,34 @@
+#' make_biodiversity_img_tag 
+#'
+#' @description A fct function
+#'
+#' @return The return value, if any, from executing the function.
+#'
+#' @noRd
+make_biodiversity_img_tag <- function(ecoregion, taxon, metric, result_type, year, ns) {
+  
+  # eco_acronym <- get_ecoregion_acronym(ecoregion)
+  eco_acronym <- "NrS"
+  metric_name <- str_replace_all(tolower(metric), " ", "_")
+  file_name <- paste0(paste(eco_acronym, taxon, metric_name, result_type, year, sep = "_"), ".png")
+  
+  # Web path used by img tag
+  webpath <- file.path("www/wp3", file_name)
+  
+  # Filesystem path used for existence check
+  file_systempath <- app_sys("app/www/wp3", file_name)
+  
+  validate(
+    need(file.exists(file_systempath),
+         paste("No data available for",
+               metric,
+               "in the", ecoregion(), "ecoregion"))
+  )
+  
+  tags$img(
+    id = ns(paste0(paste(eco_acronym, taxon, metric_name, result_type, year, sep = "_"))),
+    src = webpath,
+    style = "width: 100%; cursor: pointer;",
+    onclick = "toggleFullScreen(this)"
+  )
+}
--- R/mod_wp3_interactive_comparison.R
+++ R/mod_wp3_interactive_comparison.R
@@ -8,27 +8,63 @@
 #'
 #' @importFrom shiny NS tagList brushedPoints
 #' @importFrom DT DTOutput renderDT datatable
-#' @importFrom dplyr filter mutate where across
-#' @importFrom bslib card layout_sidebar sidebar
+#' @importFrom dplyr filter mutate where across select summarise
+#' @importFrom tidyr pivot_longer
+#' @importFrom bslib card layout_sidebar sidebar layout_column_wrap card_body
 mod_wp3_interactive_comparison_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    card(layout_sidebar(sidebar = sidebar(selectInput(ns("year_selector"), "Select Year", choices = 2010:2020, selected = 2020)),
-                        plotOutput(outputId = ns("biodiv_compare"), 
-                                                 brush = ns("plot_brush"),
-                                                 dblclick = ns("plot_reset"), 
-                                                 height = "75vh"))),
-    card(DTOutput(outputId = ns("comparison_table")))
+    
+    card(layout_sidebar(sidebar = sidebar(selectInput(ns("year_selector"), "Select Year", choices = 2010:2020, selected = 2020),
+                                          checkboxGroupInput(ns("diversity_idx"), label = "Select diversity index", 
+                                                       choices = c("Species Richness" = "Richness",
+                                                                   "Evenness" = "evenness",
+                                                                   "Shannon Index" = "shannon",
+                                                                   "Functional Richness" = "fric",
+                                                                   "Functional Evenness" = "feve",
+                                                                   "Functional Dispersion" = "fdis",
+                                                                   "Functional Diversity" = "fdiv"),
+                                                       selected = c("Richness", "evenness", "shannon"))),
+                        uiOutput(ns("plot_panel"), fill = TRUE))),
+    card(fluidRow(column(width = 4, 
+                         card(card_header("Drag to select, double-click to deselect"),
+                              card_body(padding = 0, plotOutput(outputId = ns("biodiv_compare"), 
+                                         brush = ns("plot_brush"),
+                                         dblclick = ns("plot_reset"), 
+                                         height = "35vh")))),
+                  column(width = 8, 
+                         card(card_header("Summary Statistics"),
+                         DTOutput(ns("summary_dt")))))),
+    card(DTOutput(outputId = ns("detail_dt")))
   )
 }
     
 #' wp3_interactive_comparison Server Functions
 #'
 #' @noRd 
-mod_wp3_interactive_comparison_server <- function(id, map_parameters){
+mod_wp3_interactive_comparison_server <- function(id, map_parameters, case_study){
   moduleServer(id, function(input, output, session){
     ns <- session$ns
-
+    
+    output$plot_panel <- renderUI({
+      
+      cards <- list()
+      
+      for(i in seq_along(input$diversity_idx)) {
+        cards[[i]] <- card(min_height = "25vh",
+                           card_header(toupper(input$diversity_idx[i])),
+                           card_body(padding = 0,
+                                     make_biodiversity_img_tag(ecoregion = case_study, 
+                                                     taxon = "taxon", 
+                                                     metric = input$diversity_idx[i],
+                                                     result_type = "status", 
+                                                     year = input$year_selector,
+                                                     ns = ns)))
+      }
+    
+      do.call(layout_column_wrap, c(cards, list(width = "400px")))
+    })
+    
     selected_points <- reactiveVal(rep(FALSE, nrow(fish_diversity)))
     
     observeEvent(input$plot_brush, {
@@ -59,14 +95,49 @@ mod_wp3_interactive_comparison_server <- function(id, map_parameters){
       p
     })
     
-    
-    output$comparison_table <- renderDT({
+    output$summary_dt <- renderDT({
       req(fish_diversity)
-      dat <- fish_diversity %>%
+      req(input$year_selector)
+      req(sum(selected_points())>0)
+      
+      dat <- fish_diversity[selected_points(),]
+      
+      dat %>%
+        select(all_of(input$diversity_idx)) %>%
+        summarise(across(
+          everything(),
+          list(
+            Min = ~round(min(.x, na.rm = TRUE), digits = 2),
+            "25th Percentile" = ~round(quantile(.x, digits = 2)[2]),
+            Median = ~round(quantile(.x, digits = 2)[3]),
+            "75th Percentile" = ~round(quantile(.x, digits = 2)[4]),
+            Max = ~round(max(.x, na.rm = TRUE), digits = 2),
+            Mean = ~round(mean(.x, na.rm = TRUE),  digits = 2),
+            SD = ~round(sd(.x, na.rm = TRUE), digits = 2)
+          )
+        )) %>%
+        pivot_longer(
+          cols = everything(),
+          names_to = c("Metric", ".value"),
+          names_sep = "_"
+        ) %>%
+        datatable(options = list(dom =""),
+                  rownames = FALSE)
+          
+    })
+    
+    output$detail_dt <- renderDT({
+      req(fish_diversity)
+      req(input$year_selector)
+      req(selected_points)
+      
+      dat <- fish_diversity[selected_points(),]
+      res <- fish_diversity %>%
+        select(c(Year, Cell, longitude, latitude, Richness, shannon, evenness, fric, feve, fdis, fdiv)) %>% 
         filter(Year == input$year_selector) %>% 
-        mutate(across(where(is.double) & !c(longitude, latitude, Lon_c, Lat_c), ~round(.x, digits = 1))) %>% 
-        mutate(across(c(longitude, latitude, Lon_c, Lat_c), ~round(.x, digits = 4)))
-      res <- dat[selected_points(),]
+        mutate(across(where(is.double) & !c(longitude, latitude), ~round(.x, digits = 1))) %>% 
+        mutate(across(c(longitude, latitude), ~round(.x, digits = 4)))
+      
       datatable(res)
     })
     
