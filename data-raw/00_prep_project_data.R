b7963ffcf2068c3719a4d45c971d4f756eaa297c
Neil Maginnis <neil.maginnis@ices.dk>
2026-03-13 14:34:09 +0100

    use www directory rather than img for storing images

--- data-raw/00_prep_project_data.R
+++ data-raw/00_prep_project_data.R
@@ -1,6 +1,8 @@
 # Top level script to prepare project data. Run this script to generate the data structures necessary for the app to run.
 # Sources scripts specific to individual work packages as well as scripts preparing data for the app generally.
 
+# Fetch output of data prep TAF repo
+TAF::cp("../b_useful_dst/output/wp3/*", to = "inst/app/www/wp3")
 
 # Prep WP2
 source("data-raw/prep_colours.R")
@@ -11,3 +13,4 @@ cat("Project texts prepared\n")
 
 # source("data-raw/wp2/prep_data_wp3.R")
 # cat("WP3 data prepared\n")
+
