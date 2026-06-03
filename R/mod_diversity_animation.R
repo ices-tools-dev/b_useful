8d26844f0802470d9e26edc46c5af755c1ee374c
Neil Maginnis <neil.maginnis@ices.dk>
2026-06-03 15:34:43 +0200

    Add diversity animation module

---
+++ R/mod_diversity_animation.R
@@ -0,0 +1,47 @@
+#' diversity_animation UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_diversity_animation_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    card_body(padding = 0,
+              min_height = "50vh",
+              withSpinner(uiOutput(outputId = ns("biodiv_animation"))),
+    card("Figure Information", 
+         uiOutput(ns("fig_text_anim")), 
+         height = "20vh"))
+  )
+}
+    
+#' diversity_animation Server Functions
+#'
+#' @noRd 
+mod_diversity_animation_server <- function(id, case_study, taxon, diversity_idx){
+  moduleServer(id, function(input, output, session){
+    ns <- session$ns
+ 
+    output$biodiv_animation <- renderUI({
+      req(diversity_idx())
+      req(taxon())
+      eco_acronym <- region_codes[names(region_codes) == case_study()]
+      metric_name <- str_replace_all(tolower(diversity_idx()), " ", "_")
+      file_name <- paste0(paste(eco_acronym, taxon(), metric_name, "status", sep = "_"), ".gif")
+      
+      make_img_tag(filename = file_name,
+                   ns = ns)
+    }) %>% bindCache(diversity_idx(), case_study(), taxon())
+    
+  })
+}
+    
+## To be copied in the UI
+# mod_diversity_animation_ui("diversity_animation_1")
+    
+## To be copied in the server
+# mod_diversity_animation_server("diversity_animation_1")
