7051135c9f6be6acba0c0ec984d03ec830e971d5
Neil Maginnis <neil.maginnis@ices.dk>
2026-09-13 16:49:14 +0200

    remove shapefile generation from this repo

--- data-raw/map_shapefile.R
+++
@@ -1,7 +0,0 @@
-## code to prepare `map_shapefile` dataset goes here
-library(sf)
-
-map_shape <- sf::st_read(dsn = "data-raw/shape_worldmap_simplified",
-                         layer = "world_map_simplified")
-
-usethis::use_data(map_shape, overwrite = TRUE)
